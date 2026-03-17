d684558cd17e5573881ece8f3558a9d3d1b1b458
raphaeldevelopment <135951984+raphaeldevelopment@users.noreply.github.com>
2026-03-17 17:18:17 +0200

    Fixed import

--- test/unit/EffectsStack.test.js
+++ test/unit/EffectsStack.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect, beforeEach } from "vitest";
-import { EffectsStack } from "../../src/utils/EffectsStack.js";
+import { EffectsStack } from "../../src/effects/EffectsStack.js";
 
 describe("EffectsStack", () => {
     beforeEach(() => {
